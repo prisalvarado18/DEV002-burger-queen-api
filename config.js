996c7dc5162d1bd56e8ebb43773a68759c608967
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-03-10 12:36:13 -0500

    Updating data

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ exports.port = process.argv[2] || process.env.PORT || 8080;
 exports.dbUrl = 'mongodb+srv://odsadmin:odsadmin129@company.hnbpcgq.mongodb.net/db' || 'mongodb://localhost:27017/test';
 // exports.dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/test';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
-exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
-exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
+exports.adminEmail = process.env.ADMIN_EMAIL || 'scanas@cara.com';
+exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme123';
