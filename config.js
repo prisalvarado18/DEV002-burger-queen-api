741495f351e161427268f149831e1acb1a4dcad7
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-03-10 12:44:33 -0500

    Added changes to cofig

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ exports.dbUrl = 'mongodb+srv://odsadmin:odsadmin129@company.hnbpcgq.mongodb.net/
 // exports.dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/test';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'scanas@cara.com';
-exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme123';
+exports.adminPassword = process.env.ADMIN_PASSWORD || 'pa$$All123';
